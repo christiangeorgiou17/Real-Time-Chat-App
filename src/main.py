f9e45c9e774bab57866a59cafe7f425d1b46ba3e
Christian Georgiou <christiangeorgiou17@gmail.com>
2026-08-19 11:29:46 +0100

    Set up .env & Flask initalisation.
    Set up Flask as app, CORS and JWTManager. Created .env and included JWT_SECRET_KEY.

--- src/main.py
+++ src/main.py
@@ -5,3 +5,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+app.config["JWT_SECRET_KEY"] = getenv("JWT_SECRET_KEY")
+
+jwt = JWTManager(app)
+
+
