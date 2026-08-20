bcd24524043066930c6321a7afda0776b399eadf
Christian Georgiou <christiangeorgiou17@gmail.com>
2026-08-20 08:01:38 +0100

    Database Setup Code
    Installed SQLAlchemy, database.py has database initilisation code & main.py has SQLAlchemy configs & User class for sql tables. Updated .gitignore to include database files and folders.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,39 @@
+from datetime import timedelta
 from os import getenv
 
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 CORS(app)
 
 load_dotenv()
+app.config["SQLALCHEMY_DATABASE_URL"]
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = getenv("JWT_SECRET_KEY")
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 
+
+db = SQLAlchemy(app)
 jwt = JWTManager(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
 
 
