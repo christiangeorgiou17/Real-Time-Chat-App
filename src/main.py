50e81227657afdc0daffbea5533d1c6c7cdeaae8
Christian Georgiou <christiangeorgiou17@gmail.com>
2026-08-21 09:05:57 +0100

    SQLALCHEMY_DATABASE_URI Bugfix
    SQLALCHEMY_DATABASE_URI was written as SQLALCHEMY_DATABASE_URL and has been changed, alongside the environment variable identifier.

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 load_dotenv()
-app.config["SQLALCHEMY_DATABASE_URL"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = getenv("JWT_SECRET_KEY")
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
